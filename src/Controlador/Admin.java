5d1c6852336934720db4dc929b9cd6674b09603a
Juan Carlos <juancho131401@gmail.com>
2019-04-04 17:22:36 -0600

    Quite esas madres que dijo Cianca
    asdasda

--- src/Controlador/Admin.java
+++ src/Controlador/Admin.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-public class Admin implements Serializable {
+public class Admin {
     private String Username;
     private String password;
     private boolean isAdmin = true;
@@ -45,32 +45,5 @@ public class Admin implements Serializable {
         isAdmin = admin;
     }
     
-    public void setFileAdmin(){
-        String archivo = "admins.dat";
-        try{
-            ObjectOutputStream escritura = new ObjectOutputStream(new FileOutputStream(archivo));
-            escritura.writeObject(this);
-            escritura.close();
-        }catch(FileNotFoundException ex){
-            ex.printStackTrace();
-        }catch(IOException e){
-            e.printStackTrace();
-        }
-    }
     
-    public Admin getFileAdmin(){
-        String archivo = "admins.dat";
-        try{
-            ObjectInputStream lectura = new ObjectInputStream(new FileInputStream(archivo));
-            Admin salida = (Admin) lectura.readObject();
-            lectura.close();
-            return salida;
-        }catch(IOException e){
-            e.printStackTrace();
-            return null;
-        }catch(ClassNotFoundException ex){
-            ex.printStackTrace();
-            return null;
-        }
-    } 
 }
